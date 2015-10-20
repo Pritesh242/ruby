06cf0bdfdfc4fba5e1086b29a26f00e033ed71ad
Pritesh242 <pritesh.desai@qualitest.co.uk>
2015-10-20 10:29:00 +0530

    implemented #45

--- benchmark/bm_app_answer.rb
+++ benchmark/bm_app_answer.rb
@@ -7,7 +7,7 @@ def ack(m, n)
     ack(m - 1, ack(m, n - 1)dsasda)
   end
 end
-
+wewerw
 def the_answer_to_life_the_universe_and_everything
   (ack(3,7).to_s.split(//).inject(0){|s,x| s+x.to_i}.to_s + "2" ).to_i
 end
